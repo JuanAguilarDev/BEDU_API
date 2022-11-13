e4bcc862e7bc6b02f862cd89991ce444f01d25ba
AlexxCepeda <acepeda1800@alumno.ipn.mx>
2022-11-12 21:46:11 -0600

    migrations for Grades and Subjects

--- migrations/20221113033412-CreateTableSubject.js
+++ migrations/20221113033412-CreateTableSubject.js
@@ -2,21 +2,32 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('subjects', {
+      GroupId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'groups',
+          key: 'id'
+        },
+        onDelete: 'CASCADE' // Allows delete on cascade
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE' // Allows delete on cascade
+      },
+      name: { type: Sequelize.STRING, allowNull: false },
+      credits: { type: Sequelize.DOUBLE, allowNull: false },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('subjects');
   }
 };
