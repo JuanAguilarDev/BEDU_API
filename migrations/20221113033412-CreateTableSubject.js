d5a62f746064420a1f289dc54936141bc9d3abc7
JuanDEV <juan_aguilar1290@outlook.es>
2022-11-12 21:53:09 -0600

    Migrations

--- migrations/20221113033412-CreateTableSubject.js
+++ migrations/20221113033412-CreateTableSubject.js
@@ -4,6 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('subjects', {
+      id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
       GroupId: {
         type: Sequelize.INTEGER,
         references: {
--- migrations/20221113034955-CreateTableGrade.js
+++ migrations/20221113034955-CreateTableGrade.js
@@ -4,6 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('grades', {
+      id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
       SubjectId: {
         type: Sequelize.INTEGER,
         references: {
